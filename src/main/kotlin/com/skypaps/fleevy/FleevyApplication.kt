4545f9536267b18e8f2828310131330605bab5eb
jlnppt <julien.paput@resilience.care>
2023-09-13 16:17:43 +0200

    add home

--- src/main/kotlin/com/skypaps/fleevy/FleevyApplication.kt
+++ src/main/kotlin/com/skypaps/fleevy/FleevyApplication.kt
@@ -2,9 +2,19 @@ package com.skypaps.fleevy
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
+@Controller
 @SpringBootApplication
 class FleevyApplication
+
+@RequestMapping("/")
+@ResponseBody
+fun home(){
+    "Hello world"
+}
 
 fun main(args: Array<String>) {
     runApplication<FleevyApplication>(*args)
